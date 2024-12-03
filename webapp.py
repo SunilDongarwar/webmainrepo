1cdf29bc90b38a9ed1ae6970aea772b2d1816c66
SunilDongarwar <dongarwar.sunil@gmail.com>
2024-12-04 02:46:55 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -15,9 +15,9 @@ app.secret_key = 'your_secret_key_here'  # Replace with your actual secret key
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'  # Use your email provider's SMTP server
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'your_email@gmail.com'  # Your email address
-app.config['MAIL_PASSWORD'] = 'your_app_password'  # Your email password or app password
-app.config['MAIL_DEFAULT_SENDER'] = 'your_email@gmail.com'  # Default sender
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')  # Your email address
+app.config['MAIL_PASSWORD'] =  os.getenv('MAIL_PASSWORD')  # Your email password or app password
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')  # Default sender
 
 mail = Mail(app)
 
