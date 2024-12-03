e88629917f344ae3c2a75cf19dbe86321c1bc530
SunilDongarwar <dongarwar.sunil@gmail.com>
2024-12-04 01:38:04 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
-@app.route("/")
-def helloworld():
-    return "Hello World! Lets Start Contributing Myapp, will it work ???"
-if __name__ == "__main__":
-    app.run()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
