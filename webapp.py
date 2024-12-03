90ac1c87a8e60af95c9b5e12cd38e8a85292636f
SunilDongarwar <dongarwar.sunil@gmail.com>
2024-12-03 21:29:12 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,23 +1,32 @@
 import os
 
-from flask import Flask, render_template
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
 
 app = Flask(__name__)
 
-# Define the home route
+
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
 
-# Define the contact route
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-# Define the about route
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run()
