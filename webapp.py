b4b2c51103d3290114d7d11b294052df0dc6996d
SunilDongarwar <dongarwar.sunil@gmail.com>
2024-12-03 20:50:08 +0530

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Define the home route
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# Define the contact route
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+# Define the about route
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
